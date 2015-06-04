e3015c74aa961e40064ce8efec0c24dfa134d88f
benjamin <benjamin@devforge>
2015-06-04 23:20:29 +1000

    Update

---
+++ src/Forge/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Forge;
+
+class Config {
+	protected $xml;
+
+	public function __construct($file = false) {
+		$xml = simplexml_load_string(@file_get_contents($file));
+		$json = json_encode($xml);
+		$this->xml = json_decode($json, TRUE);
+		return $this->xml;
+	}
+}
